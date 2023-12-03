7b23a3ac1673d6b29ce72b9bba11dec1c29e426b
Alexander <DrA1ex@users.noreply.github.com>
2023-12-04 01:47:20 +0500

    Refactoring

--- src/hardware.h
+++ src/hardware.h
@@ -4,11 +4,9 @@
 #include <DHT.h>
 #include <Max72xxPanel.h>
 
-#define PIN_SPEAKER 15
-#define PIN_MATRIX_CS 5
 #define PIN_DHT 4
-
-DHT dht(PIN_DHT, DHT11);
+#define PIN_MATRIX_CS 5
+#define PIN_SPEAKER 15
 
 const int numberOfHorizontalDisplays = 1;
 const int numberOfVerticalDisplays = 1;
@@ -16,4 +14,5 @@ const int spacer = 1;
 const int width = 5 + spacer;
 const int height = 8;
 
-Max72xxPanel matrix = Max72xxPanel(PIN_MATRIX_CS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
+static Max72xxPanel matrix = Max72xxPanel(PIN_MATRIX_CS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
+static DHT dht(PIN_DHT, DHT11);
